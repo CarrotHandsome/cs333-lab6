be5c52c044279970c17012d350d683231f38f164
Jacob Billings <jbill@pdx.edu>
2026-05-30 18:41:15 -0700

    socket connection working for one client

---
+++ Makefile
@@ -0,0 +1,27 @@
+CFLAGS = -g -Wall #-Wextra -Wshadow -Wunreachable-code -Wredundant-decls -Wmissing-declarations -Wold-style-definition -Wmissing-prototypes -Wdeclaration-after-statement -Wno-return-local-addr -Wunsafe-loop-optimizations -Wuninitialized -Werror -Wno-unused-parameter 
+
+PROGS = eoraptor_server eoraptor_client
+
+all: $(PROGS)
+
+eoraptor_server: eoraptor_server.o
+	gcc $(CFLAGS) -o eoraptor_server eoraptor_server.o
+
+eoraptor_server.o: eoraptor_server.c eoraptor_ui.h
+	gcc $(CFLAGS) -c -g -o eoraptor_server.o eoraptor_server.c
+
+eoraptor_client: eoraptor_client.o eoraptor_ui.o
+	gcc $(CFLAGS) -o eoraptor_client eoraptor_client.o eoraptor_ui.o -lncurses
+
+eoraptor_client.o: eoraptor_client.c eoraptor_ui.h
+	gcc $(CFLAGS) -c -o eoraptor_client.o eoraptor_client.c 
+
+eoraptor_ui.o: eoraptor_ui.c eoraptor_ui.h
+	gcc $(CFLAGS) -c -o eoraptor_ui.o eoraptor_ui.c
+
+clean cls: 
+	rm -f $(PROGS) *.o *~ \#* .*.swp
+
+run:
+	./eoraptor_server
+
---
+++ eoraptor_server.c
@@ -0,0 +1,145 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <signal.h>
+#include <libgen.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <ncurses.h>
+#include <stdlib.h>
+#include "eoraptor_ui.h"
+
+#define OPTIONS "P:vh"
+#define BUF_SIZE 1024
+#define MAX_PLAYERS 4
+#define REGISTER_CHAR '\r'
+
+void sigint_handler(int sig);
+int find_client(struct sockaddr_in cli, struct sockaddr_in registered[], int numcli);
+
+volatile sig_atomic_t keep_running = true;
+static bool is_verbose = false;
+
+int main(int argc, char * argv[]) {
+    moon_state_t moon;
+
+    int opt = 0;
+
+    int port = DEFAULT_PORT;
+    int socketfd = -1;
+    int num_players = 0;
+    struct sockaddr_in servaddr;
+    struct sockaddr_in cliaddrs[MAX_PLAYERS];
+    socklen_t addrlens[MAX_PLAYERS];// = sizeof(struct sockaddr_in);
+    player_t players[MAX_PLAYERS];
+    struct sockaddr_in incoming_cli;
+    
+    socklen_t incoming_addrlen = sizeof(struct sockaddr_in);
+    ssize_t received_bytes = 0;
+    char recv_buffer[BUF_SIZE];    
+    int current_player = 0;
+
+    struct sigaction sa;
+    sa.sa_handler = sigint_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);    
+
+    {
+        while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+            switch (opt) {
+                case 'P':      
+                    sscanf(optarg, "%d", &port);              
+                    break;
+                case 'v':
+                    is_verbose = true;
+                    break;
+                case 'h':
+                    fprintf(stdout, "Helpful hints\n");
+                    exit(EXIT_SUCCESS);
+                    break;
+            }
+            
+        }
+    }
+
+    /* initialize socket */
+    if ((socketfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("socket creation error");
+        return(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(struct sockaddr_in));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(socketfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) == -1) {
+        perror("bind failure");
+        return(EXIT_FAILURE);
+    }
+
+    if (is_verbose > 0) {
+        printf("Listening for connections on port %d\n", port);
+    }
+
+    while(keep_running) {
+        received_bytes = recvfrom(socketfd, recv_buffer, BUF_SIZE, 0
+            , (struct sockaddr *) &incoming_cli, &incoming_addrlen);
+        
+        if (received_bytes == -1) {
+            continue;
+        }
+
+        if (recv_buffer[0] == REGISTER_CHAR) {
+            if (num_players >= MAX_PLAYERS) {
+                if (is_verbose) {
+                    fprintf(stderr, "Too many clients. Registration failed.\n");
+                }
+                continue;
+            }
+            cliaddrs[num_players] = incoming_cli;
+            addrlens[num_players] = incoming_addrlen;
+            ++num_players;
+
+            if (is_verbose) {
+                fprintf(stderr, "New client registered\n");
+            }
+            continue;
+        }
+
+        if ((current_player = find_client(incoming_cli, cliaddrs, num_players)) == -1) {
+            continue;
+        }
+
+        //now we have a valid client and can process the message
+        printf("%c\n", recv_buffer[0]);
+
+
+
+    }
+
+}
+
+//return index of registered client or -1 if not registered
+int find_client(struct sockaddr_in cli, struct sockaddr_in registered[], int numcli) {
+    for (int i = 0; i < numcli; ++i) {
+        if (registered[i].sin_addr.s_addr == cli.sin_addr.s_addr
+            && registered[i].sin_port == cli.sin_port) {
+            return i;
+        }
+    }
+
+    if (is_verbose) {
+        fprintf(stderr, "Unregistered client needs to send '\r' to register first\n");
+    }
+    return -1;
+}
+
+void sigint_handler(int sig) {
+    keep_running = false;
+}
